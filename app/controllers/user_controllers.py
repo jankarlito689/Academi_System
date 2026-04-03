d126cb36c7649354e1399190e84dd565a76c6c5b
cesar jan karlo <leossuarez103@gmail.com>
2026-04-03 15:36:53 -0600

    se implemento el schemal

--- app/controllers/user_controllers.py
+++ app/controllers/user_controllers.py
@@ -2,28 +2,34 @@ from fastapi import HTTPException
 from app.service.user_service import list_users, get_user, create_new_user, update_existing_user, delete_existing_user
 
 def get_users_controller():
-    return list_users()
+    result = list_users()
+    return result.data
 
 def get_user_controller(user_id: int):
+    user = get_user(user_id)
+    if not user.data:
+        raise HTTPException(status_code=404, detail="User not found")
+
+    return user.data
+def create_user_controller(data):
     try:
-        return get_user(user_id)
-    except Exception as e:
-        raise HTTPException(status_code=404, detail=str(e))
-    
-def create_user_controller(data: dict):
-    try:
-        return create_new_user(data)
+        result = create_new_user(data.dict())
+        return result.data[0]   # Retorna el nuevo usuario creado
     except Exception as e:
         raise HTTPException(status_code=404, detail=str(e))
 
-def update_user_controller(user_id: int, data: dict):
+def update_user_controller(user_id: int, data):
     try:
-        return update_existing_user(user_id, data)
-    except Exception as e:
-        raise HTTPException(status_code=404, detail=str(e))
+        result = update_existing_user(user_id, data.dict(exclude_unset=True))
+        return result.data[0]
+    except Exception:
+        raise HTTPException(status_code=500, detail="Error updating user")
+
 
 def delete_user_controller(user_id: int):
-    try:
-        return delete_existing_user(user_id)
-    except Exception as e:
-        raise HTTPException(status_code=404, detail=str(e))
+    result = delete_existing_user(user_id)
+
+    if not result.data:
+        raise HTTPException(status_code=404, detail="User not found")
+
+    return {"message": "User deleted successfully"}
--- app/routes/user_routes.py
+++ app/routes/user_routes.py
@@ -1,22 +1,31 @@
 from fastapi import APIRouter
-from app.controllers.user_controllers import get_users_controller,get_user_controller, create_user_controller, update_user_controller, delete_user_controller
+from app.controllers.user_controllers import (
+    get_users_controller,
+    get_user_controller, 
+    create_user_controller, 
+    update_user_controller, 
+    delete_user_controller
+)
+from app.schemas.user_schema import UserCreate, UserUpdate, UserResponse
+from typing import List
+
 
 router = APIRouter(prefix="/users", tags=["Users"])
 
-@router.get("/")
+@router.get("/", response_model=List[UserResponse])
 def get_users():
     return get_users_controller()
 
-@router.get("/{user_id}")
+@router.get("/{user_id}", response_model=UserResponse)
 def get_user(user_id: int):
     return get_user_controller(user_id)
 
-@router.post("/")
-def create_user(data: dict):
+@router.post("/", response_model=UserResponse)
+def create_user(data: UserCreate):
     return create_user_controller(data)
 
-@router.put("/{user_id}")
-def update_user(user_id: int, data: dict):
+@router.put("/{user_id}", response_model=UserResponse)
+def update_user(user_id: int, data: UserUpdate):
     return update_user_controller(user_id, data)
 
 @router.delete("/{user_id}")
