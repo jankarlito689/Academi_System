09b92bbd07ba4d565d40412ba91389b7f858996a
cesar jan karlo <leossuarez103@gmail.com>
2026-04-04 09:30:24 -0600

    se integro el exepciones

--- app/controllers/user_controllers.py
+++ app/controllers/user_controllers.py
@@ -1,5 +1,7 @@
-from fastapi import HTTPException
+#controllers/user_controllers.py
 from app.service.user_service import list_users, get_user, create_new_user, update_existing_user, delete_existing_user
+from app.utils.exceptions import not_found,server_error
+
 
 def get_users_controller():
     result = list_users()
@@ -8,28 +10,29 @@ def get_users_controller():
 def get_user_controller(user_id: int):
     user = get_user(user_id)
     if not user.data:
-        raise HTTPException(status_code=404, detail="User not found")
+        not_found("User not found")
 
     return user.data
+
 def create_user_controller(data):
     try:
         result = create_new_user(data.dict())
         return result.data[0]   # Retorna el nuevo usuario creado
-    except Exception as e:
-        raise HTTPException(status_code=404, detail=str(e))
+    except Exception:
+        server_error("Error creating user")
 
 def update_user_controller(user_id: int, data):
     try:
         result = update_existing_user(user_id, data.dict(exclude_unset=True))
         return result.data[0]
     except Exception:
-        raise HTTPException(status_code=500, detail="Error updating user")
+        server_error("Error updating user")
 
 
 def delete_user_controller(user_id: int):
     result = delete_existing_user(user_id)
 
     if not result.data:
-        raise HTTPException(status_code=404, detail="User not found")
+        not_found("User not found")
 
     return {"message": "User deleted successfully"}
