f7578db5a1548b3ab50b33e7e1f018ab1574f9b9
cesar jan karlo <leossuarez103@gmail.com>
2026-04-08 19:44:11 -0600

    se creo la capa user dentro schemas

---
+++ app/schemas/users/user_schema.py
@@ -0,0 +1,31 @@
+from pydantic import BaseModel, EmailStr
+from typing import Optional
+
+# crear usuario
+class UserCreate(BaseModel):
+    name: str
+    email: EmailStr
+    phone: str
+    password_hash: str
+    rol: str
+    academia_id: int
+
+# actualización
+class UserUpdate(BaseModel):
+    name: Optional[str] = None
+    email: Optional[EmailStr] = None
+    phone: Optional[str] = None
+    password_hash: Optional[str] = None
+    rol: Optional[str] = None
+
+# salida
+class UserResponse(BaseModel):
+    id: int
+    name: str
+    email: EmailStr
+    phone: str
+    rol: Optional[str] = None
+    academia_id: Optional [int] = None
+
+    class Config:
+        from_attributes = True
