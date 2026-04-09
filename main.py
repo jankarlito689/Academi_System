0ee101742f460edb47b00902d5bbe00b798cbdba
cesar jan karlo <leossuarez103@gmail.com>
2026-04-08 19:45:56 -0600

    se integro la ruta

--- main.py
+++ main.py
@@ -1,8 +1,11 @@
 from fastapi import FastAPI
 from app.routes.user_routes import router as user_router
+from app.routes.academy_routes import router as academy_router
+
 app = FastAPI()
 
 app.include_router(user_router)
+app.include_router(academy_router)
 
 @app.get("/")
 
