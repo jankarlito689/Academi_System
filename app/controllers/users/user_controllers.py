69e77145cc2b415898de859b5c663ec0a7e7d828
cesar jan karlo <leossuarez103@gmail.com>
2026-04-08 19:45:08 -0600

    se creo la capa user dentro controllers

---
+++ app/controllers/users/user_controllers.py
@@ -0,0 +1,37 @@
+#controllers/user_controllers.py
+from app.service.users.user_service import list_users, get_user, create_new_user, update_existing_user, delete_existing_user
+from app.utils.exceptions import not_found,server_error
+
+def get_users_controller():
+    result = list_users()
+    return result.data
+
+def get_user_controller(user_id: int):
+    user = get_user(user_id)
+    if not user.data:
+        not_found("User not found")
+
+    return user.data
+
+def create_user_controller(data):
+    try:
+        result = create_new_user(data.dict())
+        return result.data[0]   # Retorna el nuevo usuario creado
+    except Exception:
+        server_error("Error creating user")
+
+def update_user_controller(user_id: int, data):
+    try:
+        result = update_existing_user(user_id, data.dict(exclude_unset=True))
+        return result.data[0]
+    except Exception:
+        server_error("Error updating user")
+
+
+def delete_user_controller(user_id: int):
+    result = delete_existing_user(user_id)
+
+    if not result.data:
+        not_found("User not found")
+
+    return {"message": "User deleted successfully"}
