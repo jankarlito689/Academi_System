b22a99c5361a8638c208abe05f25a78865f92834
cesar jan karlo <leossuarez103@gmail.com>
2026-04-11 14:45:03 -0600

    se agrego la new ruta

--- main.py
+++ main.py
@@ -1,12 +1,14 @@
 from fastapi import FastAPI
 from app.routes.user_routes import router as user_router
 from app.routes.academy_routes import router as academy_router
+from app.routes.auth_routes import router as auth_router
+
 
 app = FastAPI()
 
 app.include_router(user_router)
 app.include_router(academy_router)
-
+app.include_router(auth_router)
 @app.get("/")
 
 def root():
